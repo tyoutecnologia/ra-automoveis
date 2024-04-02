dbf0ef8df3f75cd290d0acb47dc48e5dd3653461
João Pedro Caetano <106156212+caetanojpo@users.noreply.github.com>
2024-04-02 00:21:27 -0300

    colors and assets

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,12 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const colors = {
-    regular: "#464748",
-    light: "#A9ABAE",
-    lineLight: "#D2D3D5",
-    radiant: "#71625B",
-    sublime: "#F7ABAB",
-    textHighlight: "#B38E46",
+    primary: "#CB3438",
+    secondary: "#843836",
+    dark: "#282829",
+    light:"#FEFEFE",
+    lightGray: "#E6E7E8"
+
   };
   
   export const theme = extendTheme({ colors });
